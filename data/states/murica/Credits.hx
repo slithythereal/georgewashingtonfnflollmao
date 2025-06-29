ee1b443434ec2612c3cabe7561e1f63a23053b95
MRmorian <rianhendrickribeiromrmorian@gmail.com>
2025-06-29 19:17:32 -0300

    Credits tags + recoded

--- data/states/murica/Credits.hx
+++ data/states/murica/Credits.hx
@@ -9,13 +9,6 @@ var credits:Array<{name:String, link:String, desc:String}> = [
 	// {name: "micahstuff", link: "", desc: "music guy\nGrimace"},
 	// {name: "kaegan1636", link: "", desc: "music guy\nDementia"},
 	{name: 'capitnparrot', link: 'https://www.youtube.com/channel/UC08fJSpXa97QeISAYoYftgg', desc: 'moral support\njust the GOAT all around'},
-	{name: 'marquis artuis', link: 'https://twitter.com/MarquisArtuis', desc: 'trump icons'},
-	{name: 'g-nux', link: "https://g-nux.newgrounds.com", desc: "mod thumbnail"},
-	{name: 'rodney528', link: "https://gamebanana.com/members/1729833", desc: 'Change Character Script\nin CNE discord'},
-	{name: "hifish", link: "https://twitter.com/hifish__", desc: "Installer batchfile (.bat) script"},
-	{name: 'rinsai', link: 'https://twitter.com/Rinsai_1', desc: "Eagle Fortnite default dance\n\nI TRIED FINDING IT EVERYWHERE,\nTHANK GOD SOMEONE HAD IT"},
-	{name: 'vsgorefield', link: "https://gamebanana.com/mods/501201", desc: "i stole your videohandler script"},
-	{name: 'dovlin', link: "https://scarletviolet.pokemon.com/en-us/", desc: "he's finally a reference..."}
 ];
 
 var specialThanks:Array<{name:String, link:String, desc:String}> = [
@@ -29,9 +22,11 @@ var specialThanks:Array<{name:String, link:String, desc:String}> = [
 ];
 
 var curSelected:Int = 0;
+var curTag:Int = 0;
+var curList = credits;
 var creditImageGrp:FlxTypedGroup<FlxSprite>;
 var georgeScroll, arrowDOWN, arrowUP, credIcon:FlxSprite;
-var credName, credDescTxt:FlxText;
+var credName, credDescTxt, specialTxt:FlxText;
 
 function create() {
 	var isBrazil:Bool = (FlxG.save.data.mailRead.contains("brazil") && FlxG.save.data.curCountry == 'brazil');
@@ -51,20 +46,20 @@ function create() {
 	add(georgeScroll);
 
 	credIcon = new FlxSprite(394, 344);
-	credIcon.loadGraphic(Paths.image('menus/credits/credIcons/' + credits[curSelected].name));
+	credIcon.loadGraphic(Paths.image('menus/credits/credIcons/' + curList[curSelected].name));
 	credIcon.scale.set(1, 1);
 	credIcon.updateHitbox();
 	add(credIcon);
 
 	credName = new FlxText(394, 495);
-	credName.text = credits[curSelected].name.toUpperCase();
+	credName.text = curList[curSelected].name.toUpperCase();
 	credName.setFormat("fonts/THE PRESIDENT.ttf", 25, FlxColor.BLACK, "left");
 	credName.borderSize = 2;
 	add(credName);
 
 	credDescTxt = new FlxText(470, 360);
 	credDescTxt.fieldWidth = 500;
-	credDescTxt.text = 'Roles in thy mod\n' + credits[curSelected].desc;
+	credDescTxt.text = 'Roles in thy mod\n' + curList[curSelected].desc;
 	credDescTxt.setFormat("fonts/THE PRESIDENT.ttf", 25, FlxColor.BLACK, "center");
 	credDescTxt.borderSize = 2;
 	add(credDescTxt);
@@ -81,42 +76,65 @@ function create() {
 	arrowUP.scale.set(0.25, 0.15);
 	arrowUP.updateHitbox();
 	add(arrowUP);
+	
+	specialTxt = new FlxText(FlxG.width /2 - 250, 575);
+	specialTxt.fieldWidth = 500;
+	specialTxt.text = 'Press [TAB] to see the Special Thanks.';
+	specialTxt.setFormat("fonts/THE PRESIDENT.ttf", 20, FlxColor.BLACK, "center");
+	specialTxt.borderSize = 2;
+	add(specialTxt);
+
+	changeCred();
+	changeTag();
 }
 
 function update(elapsed:Float) {
-	if (controls.UP_P || FlxG.mouse.overlaps(arrowUP) && FlxG.mouse.justPressed) {
-		changeCred(-1);
-		FlxTween.tween(arrowUP, {"scale.x": 0.35, "scale.y": 0.25}, 0.05, {
-			ease: FlxEase.quintInOut,
-			onComplete: function(twn:FlxTween) {
-				FlxTween.tween(arrowUP, {"scale.x": 0.25, "scale.y": 0.15}, 0.05, {ease: FlxEase.quintInOut});
-			}
-		});
-	} else if (controls.DOWN_P || FlxG.mouse.overlaps(arrowDOWN) && FlxG.mouse.justPressed) {
-		changeCred(1);
-		FlxTween.tween(arrowDOWN, {"scale.x": 0.35, "scale.y": 0.25}, 0.05, {
-			ease: FlxEase.quintInOut,
-			onComplete: function(twn:FlxTween) {
-				FlxTween.tween(arrowDOWN, {"scale.x": 0.25, "scale.y": 0.15}, 0.05, {ease: FlxEase.quintInOut});
-			}
-		});
-	}
-	if (controls.ACCEPT || FlxG.mouse.overlaps(credIcon) && FlxG.mouse.justPressed)
-		CoolUtil.openURL(credits[curSelected].link); // opens credit link
-	if (controls.BACK) {
+	var upP = controls.UP_P;
+    var downP = controls.DOWN_P;
+	if (upP || downP) changeCred(0 - Std.int(upP) + Std.int(downP));
+	if (FlxG.mouse.justPressed && (FlxG.mouse.overlaps(arrowUP) || FlxG.mouse.overlaps(arrowDOWN)))changeCred(0 - Std.int(FlxG.mouse.overlaps(arrowUP)) + Std.int(FlxG.mouse.overlaps(arrowDOWN)));
+	
+	var tab = FlxG.keys.justPressed.TAB;
+	if (tab) changeTag(1);
+	
+	var accept = controls.ACCEPT;
+	if (accept || FlxG.mouse.overlaps(credIcon) && FlxG.mouse.justPressed)
+		CoolUtil.openURL(curList[curSelected].link); // opens credit link
+
+	var back = controls.BACK;
+	if (back) {
 		FlxG.sound.play(Paths.sound('menu/cancel'));
 		FlxG.switchState(new MainMenuState());
 	}
 }
 
-function changeCred(cool:Int) {
-	curSelected += cool;
-	if (curSelected >= credits.length)
-		curSelected = 0;
-	if (curSelected < 0)
-		curSelected = credits.length - 1;
+function changeCred(cool:Int = 0) {
+	curSelected = FlxMath.wrap(curSelected + cool, 0, curList.length-1);
+	var arrow = null;
+	if (cool < 0)
+		arrow = arrowUP;
+	if (cool > 0)
+		arrow = arrowDOWN;
 
-	credIcon.loadGraphic(Paths.image('menus/credits/credIcons/' + credits[curSelected].name));
-	credName.text = credits[curSelected].name.toUpperCase();
-	credDescTxt.text = 'Roles in thy mod\n' + credits[curSelected].desc;
+	if (arrow != null)
+		FlxTween.tween(arrow, {"scale.x": 0.35, "scale.y": 0.25}, 0.05, {
+			ease: FlxEase.quintInOut,
+			onComplete: function(twn:FlxTween) {
+				FlxTween.tween(arrow, {"scale.x": 0.25, "scale.y": 0.15}, 0.05, {ease: FlxEase.quintInOut});
+			}
+		});
+
+	credIcon.loadGraphic(Paths.image('menus/credits/credIcons/' + curList[curSelected].name));
+	credName.text = curList[curSelected].name.toUpperCase();
+	credDescTxt.text = 'Roles in thy mod\n' + curList[curSelected].desc;
+}
+
+function changeTag(num:Int = 0)
+{
+	curTag = FlxMath.wrap(curTag + num, 0, 1);
+	var cycle = [[credits, 'Press [TAB] to see the Special Thanks.'], [specialThanks, 'Press [TAB] to see the Credits.']];
+	curList = cycle[curTag][0];
+	specialTxt.text = cycle[curTag][1];
+	curSelected = 0;
+	changeCred();
 }
